d1184705cc497a720657ab54e2e3e997d7aca38e
hiroyuki tamura <tamura@e-grant.net>
2016-04-20 16:54:42 +0900

    trailing ; added

--- hoge.java
+++ hoge.java
@@ -1,11 +1,11 @@
 class hoge{
     public static void main(String args[])
     {
-	System.out.println("hogehoge fugafuga piyopiyo")
+	System.out.println("hogehoge fugafuga piyopiyo");
     }
 
     void about(){
-	System.out.println("about this program, (some|any)thing")
+	System.out.println("about this program, (some|any)thing");
     }
 
     void help(){
